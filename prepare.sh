0dfb951c84fc7647367f394d343b4b534babd806
philipp1992 <mail@pm92.de>
2024-02-07 10:38:04 +0100

    fix docker installation

--- prepare.sh
+++ prepare.sh
@@ -1,22 +1,15 @@
-#/bin/bash
-
-sudo apt-get install -y jq curl bc 
-sudo apt-get install -y gnupg
-sudo apt-get install -y ca-certificates
-sudo apt-get install -y lsb-release
-
-### Download the docker gpg file to Ubuntu
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-### Add Docker and docker compose support to the Ubuntu's packages list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginsudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginlinux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-
+# Add Docker's official GPG key:
 sudo apt-get update
- 
-### Install docker and docker compose on Ubuntu
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
- 
-### Verify the Docker and docker compose install on Ubuntu
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
